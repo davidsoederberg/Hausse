cd4c89165ecb140d1313ac42b3f209e67962bddb
David Söderberg <davidsoderberg@comhem.se>
2018-09-18 13:43:29 +0200

    Added builder for stockObjects and only accepting US or SE at the moment

--- scripts/search.js
+++ scripts/search.js
@@ -1,19 +1,19 @@
 const request = require('request');
 
 exports.search = function(stockName) {
-    return new Promise(function(resolve) {
+    return new Promise(function(resolve, reject) {
         request(`https://www.avanza.se/_mobile/market/search/STOCK?query=${stockName}`, function(error, response, body) {
             if(error) {
                 console.log(error);
             }
             else{
                 const result = [];
-                const stockArray = JSON.parse(body).hits[0].topHits.slice(0, 2);
+                const stockArray = JSON.parse(body).hits[0].topHits.slice(0, 1);
                 stockArray.forEach((stock, index) => {
-                    console.log(stock);
-                    const stockObject = {};
-                    stockObject.ticker = stock.tickerSymbol.replace(' ', '-');
-                    stockObject.name = stock.name;
+                    const stockObject = buildStockObject(stock);
+                    if(!stockObject) {
+                        reject();
+                    }
                     result[index] = stockObject;
                 });
                 resolve(result);
@@ -21,3 +21,20 @@ exports.search = function(stockName) {
         });
     });
 };
+
+function buildStockObject(stock) {
+    console.log(stock);
+    const stockObject = {};
+    if(stock.flagCode === 'US' || stock.flagCode === 'SE') {
+        stockObject.name = stock.name;
+        stockObject.currency = stock.currency;
+        stockObject.ticker = stock.tickerSymbol.replace(' ', '-');
+        if(stock.flagCode === 'SE') {
+            stockObject.ticker = stockObject.ticker.concat('.st');
+        }
+        return stockObject;
+    }
+    else {
+        return false;
+    }
+}
