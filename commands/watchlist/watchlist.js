9a0e93a1a830b074e4b866468d05e29431864514
David Söderberg <davidsoderberg@comhem.se>
2018-09-30 02:14:33 +0200

    add and remove from your watchlist with argument implemented

--- commands/watchlist/watchlist.js
+++ commands/watchlist/watchlist.js
@@ -8,30 +8,80 @@ module.exports = {
     usage: false,
     description: 'PLACEHOLDEr',
     execute(message, args) {
-        Watchlist.findOne({ userId: message.author.id })
-            .exec((err, foundWatchlist) => {
-                if(err) {
-                    console.log(err);
-                }
-                else if(!foundWatchlist) {
-                    const watchlist = new Watchlist();
-                    watchlist.userId = message.author.id;
-                    watchlist.stocks = [];
-                    watchlist.save((err, wl) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            console.log(foundWatchlist);
-                            return message.reply('Skapade en watchlist');
-                        }
+        // TODO FIX ALL MESSAGE REPLIES
+        const userId = message.author.id;
+        const findOnePromise = findOneWatchlist(userId);
+        findOnePromise.then(foundWatchlist => {
+            if(args.length === 0) {
+                if(!foundWatchlist) {
+                    const createNewPromise = createNewWatchlist(userId, null);
+                    createNewPromise.then(wl => {
+                        return message.reply(wl.stocks);
                     });
                 }
                 else {
-                    console.log(foundWatchlist);
                     return message.reply(foundWatchlist.stocks.toString());
                 }
-            });
-
+            }
+            else {
+                const stock = args[0];
+                let stocks = foundWatchlist.stocks;
+                if(stocks.indexOf(stock) > -1) {
+                    stocks = stocks.filter((element => element !== stock));
+                }
+                else {
+                    stocks.push(stock);
+                }
+                const updatePromise = findOneAndUpdateWatchlist(userId, stocks);
+                updatePromise.then(updatedWatchlist => {
+                    return message.reply(updatedWatchlist.stocks);
+                });
+            }
+        });
     },
 };
+
+async function createNewWatchlist(newUserId, stock) {
+    const watchlist = new Watchlist();
+    watchlist.userId = newUserId;
+    watchlist.stocks = stock ? [stock] : [];
+    return new Promise((resolve, reject) => {
+        watchlist.save((err, wl) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(wl);
+            }
+        });
+    });
+}
+
+async function findOneWatchlist(searchUserId) {
+    return new Promise((resolve, reject) => {
+        Watchlist.findOne({ userId: searchUserId })
+            .exec((err, foundWatchlist) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(foundWatchlist);
+                }
+            });
+    });
+}
+
+async function findOneAndUpdateWatchlist(searchUserId, newStocks) {
+    return new Promise((resolve, reject) => {
+        Watchlist.findOneAndUpdate({ userId: searchUserId }, { $set: { stocks: newStocks } },
+            { upsert: true, new: true }, (err, wl) => {
+                if(err) {
+                    reject(err);
+                }
+                else {
+                    resolve(wl);
+                }
+            });
+    });
+}
+
